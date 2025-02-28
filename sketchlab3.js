54c6c966b6e0dfc499f3a3fb63269145f40c8d14
evilkimin <maep@ocadu.ca>
2025-02-28 11:38:21 -0500

    Add files via upload

---
+++ sketchlab3.js
@@ -0,0 +1,59 @@
+var button;
+var button2;
+
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    button = createButton('catch me if you can');
+    button.position(500,440);
+    button.show()
+
+button2 = createButton('oh no you got me!!');
+    button2.position(670,800);
+    button2.hide();
+    button2.mousePressed(function(){
+        button2.hide();
+        button.show();
+    });
+
+    button.mousePressed(function(){
+        button2.show();
+        button.hide();
+
+    });
+   
+    //button2.onclick(function(){
+      //  colourChange2();
+    //})
+}
+
+function draw(){
+    background('tomato');
+    if(mouseIsPressed){
+        background('yellow')
+    }
+    text('i cant get my call backs to work!! please help', 100,100);
+   // if(mousePressed){
+  //      colourChange();
+  //  }
+}
+
+//var thecolour = 0;
+
+//function colourChange(){
+  //  thecolour = (thecolour + 1) % 3;
+    //if(thecolour === 0){
+      //  background() = 'tomato';
+    //}
+    //if(thecolour === 1){
+      //  background() = 'aquamarine';
+    //}
+    //if(thecolour === 2){
+      //  background() = 'yellow';
+    //}
+//}
+//TRIED TO MAKE CALL BACK BUT IT DIDNT WORK. ADVICE???
+
+//function colourChange2(){
+    //background('pink')
+//}
